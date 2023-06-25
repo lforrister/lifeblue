04cf7d9fde89c0ae1445b181dae60a7ae5223bb6
Lindsay Forrister <lindsayforrister@gmail.com>
2023-06-25 16:20:47 -0600

    safari testing

--- src/components/icons/EditIcon.vue
+++ src/components/icons/EditIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 24 24" width="30px" height="30px" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M12 20H20.5M18 10L21 7L17 3L14 6M18 10L8 20H4V16L14 6M18 10L14 6"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
 </template>
